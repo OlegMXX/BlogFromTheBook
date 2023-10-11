f66e2f1d03613620b07fd12216dd9ce6f5a963fb
OllyYa <exrhodo@gmail.com>
2023-10-11 15:02:39 +0300

    URL resolv, pagination at post_list

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager): # прикладной менеджер запросов
@@ -32,7 +33,9 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish') # для URL, на одну дату запрет на одинаковые слаги.
+                                                       # параметр работат на уровне Джанго, а не БД.
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -56,3 +59,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        """
+        Динамически формирует URL-адрес.
+        :return:
+        """
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
