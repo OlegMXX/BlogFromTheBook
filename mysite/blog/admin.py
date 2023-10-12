96ad19dc3297512918a2d5abf71e9be11d9c9773
OllyYa <exrhodo@gmail.com>
2023-10-12 14:06:16 +0300

    Add comments

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 # Регистрация модели Пост в админке
@@ -17,3 +17,9 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ['author'] # задавать автора с помощью его id, а не из выпадающего списка
     date_hierarchy = 'publish' # навигация по иерархии дат
     ordering = ['status', 'publish'] # критерии сортировки
+
+@ admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import CommentForm
+from django.views.decorators.http import require_POST
 
 
 # пресдставление на основе функции
@@ -30,7 +32,35 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    # Список  активных комментариев к посту
+    comments = post.comments.filter(active=True)
+    # Форма для комментариев пользоавтелями
+    form = CommentForm()
 
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    # комментарий был отправлен
+    form = CommentForm(data = request.POST)
+    if form.is_valid():
+        # Создание объекта клаасса Comment, не сохраяняя его в БД
+        # Объект создается, но его можно видоизменить перед сохранением в БД
+        comment = form.save(commit=False)
+        # назначение поста комментарию
+        comment.post = post
+        # сохраниение комментария в БД
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
+
